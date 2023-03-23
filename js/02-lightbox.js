f1650c44f7871fd1443b8b5b5d7bad9af29293f6
boomm01 <116384488+boomm01@users.noreply.github.com>
2023-03-24 01:16:01 +0200

    push

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,75 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const galeryEl = document.querySelector('.gallery');
+const galeryEl = document.querySelector(".gallery");
 
-function createGallaryMarcup (items){
-    return items.map((item) => `<li class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+function createGallaryMarcup({ preview, original, description }) {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
- </li>`).join('');
+ </li>`;
 }
 
-const addGalleryMarkup = createGallaryMarcup(galleryItems);
+const galeryCard = galleryItems.map((el) => createGallaryMarcup(el));
 
-galeryEl.insertAdjacentHTML('beforeend', addGalleryMarkup);
+galeryEl.insertAdjacentHTML("beforeend", galeryCard.join(""));
+
+galeryEl.addEventListener("click", onImageClick);
+
+
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+   captionsData: "alt",
+   captionsDelay: 250,
+ });
+   lightbox.open();
+ 
+
+
+  galeryEl.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      lightbox.close();
+    }
+  });
+}
+
+
+
+
+
+// function onImageClick(event) {
+//    event.preventDefault();
+ 
+//    if (event.target.nodeName !== "IMG") {
+//      return;
+//    }
+ 
+//    const lightbox = new SimpleLightbox(".gallery a", {
+//     captionsData: "alt",
+//     captionsDelay: 250,
+//   });
+//     lightbox.on('show.simplelightbox', function(){
+//       lightbox.open();
+//     });
+  
+ 
+ 
+//    galeryEl.addEventListener("keydown", (event) => {
+//      if (event.code === "Escape") {
+//       lightbox.on('close.simplelightbox', function(){
+//          lightbox.close();
+//        });
+//      }
+//    });
+//  }
+ 
